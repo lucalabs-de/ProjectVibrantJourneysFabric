c5ace961f60b9b3cd0f1eb8f36fe55ca7b081910
Gian Luca Spitzer <gian.luca.spitzer@rwth-aachen.de>
2025-05-10 14:58:02 +0200

    initial commit

---
+++ src/client/java/de/lucalabs/vibrantjourneys/ProjectVibrantJourneysClient.java
@@ -0,0 +1,10 @@
+package de.lucalabs.vibrantjourneys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectVibrantJourneysClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
